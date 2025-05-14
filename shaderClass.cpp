9fff6c84a7221bf83397068566889f8cd61bacd1
arped <arped@LAPTOP-R0NB0TE2>
2025-05-14 19:59:46 +0530

    Add project files.

---
+++ shaderClass.cpp
@@ -0,0 +1,95 @@
+#include "shaderClass.h"
+
+// Reads a text file and outputs a string with everything in the text file
+std::string get_file_contents(const char* filename)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (in)
+	{
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	throw(errno);
+}
+
+Shader::Shader(const char* vertexFile, const char* fragmentFile) 
+{
+	std::string vertexCode = get_file_contents(vertexFile);
+	std::string fragmentCode = get_file_contents(fragmentFile);
+
+	const char* vertexSource = vertexCode.c_str();
+	const char* fragmentSource = fragmentCode.c_str();
+
+	//Create a vertex shader object
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Attach the vertex shader source code to the object
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	//Since the GPU can't understand the source code, we compile the source code to machine code
+	glCompileShader(vertexShader);
+	// Checks if Shader compiled succesfully
+	compileErrors(vertexShader, "VERTEX");
+
+	//Do the same as above for fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	compileErrors(fragmentShader, "FRAGMENT");
+
+	// Shader program is needed to actually use these shaders
+	// Create Shader Program Object and get its reference
+	ID = glCreateProgram();
+	// Attach the Vertex and Fragment Shaders to the Shader Program
+	glAttachShader(ID, vertexShader);
+	glAttachShader(ID, fragmentShader);
+	// Wrap-up/Link all the shaders together into the Shader Program
+	glLinkProgram(ID);
+	compileErrors(ID, "PROGRAM");
+
+	// vertexShader and fragmentShader are now useless as they are already wrapped in shaderProgram
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
+// Activates the Shader Program
+void Shader::Activate()
+{
+	glUseProgram(ID);
+}
+
+// Deletes the Shader Program
+void Shader::Delete()
+{
+	glDeleteProgram(ID);
+}
+
+// Checks if the different Shaders have compiled properly
+void Shader::compileErrors(unsigned int shader, const char* type)
+{
+	// Stores status of compilation
+	GLint hasCompiled;
+	// Character array to store error message in
+	char infoLog[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_COMPILATION_ERROR for:" << type << "\n" << infoLog << std::endl;
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &hasCompiled);
+		if (hasCompiled == GL_FALSE)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			std::cout << "SHADER_LINKING_ERROR for:" << type << "\n" << infoLog << std::endl;
+		}
+	}
+}
